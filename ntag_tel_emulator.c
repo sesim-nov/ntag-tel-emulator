005b7c94f51e7984995f7e2eb40fd49a78a7e092
Aaron Moore <aaron@atamisk.net>
2023-06-23 13:48:35 -0400

    Starting boilerplate

---
+++ ntag_tel_emulator.c
@@ -0,0 +1,7 @@
+#include <furi.h>
+
+#include "ntag_tel_emulator.h"
+
+int ntag_tel_emulator_app() {
+    return 0;
+}
---
+++ ntag_tel_emulator.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <gui/view_dispatcher.h>
+#include <gui/scene_manager.h>
+
+typedef struct {
+} NtagTelEmulatorData;
+
