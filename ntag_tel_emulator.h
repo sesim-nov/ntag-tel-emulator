c907104b2436f9cf166955bc554cc22090a6776e
Aaron Moore <aaron@atamisk.net>
2023-06-23 13:52:39 -0400

    Working on data model.

--- ntag_tel_emulator.h
+++ ntag_tel_emulator.h
@@ -2,7 +2,18 @@
 
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
+#include <gui/modules/dialog_ex.h>
 
 typedef struct {
+    FuriString* nfc_file_name;
+} NtagTelEmulatorModel;
+
+typedef struct {
+    SceneManager* scene_manager;
+
+    ViewDispatcher* view_dispatcher;
+    DialogEx* dialog_ex;
+
+    NtagTelEmulatorModel* model;
 } NtagTelEmulatorData;
 
