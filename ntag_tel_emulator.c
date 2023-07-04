d76e7b5e26dc413c2e7c1bf2f775edb162edf813
Aaron Moore <1330844+sesim-nov@users.noreply.github.com>
2023-07-04 14:41:18 -0400

    Working on dummy tag.

--- ntag_tel_emulator.c
+++ ntag_tel_emulator.c
@@ -7,7 +7,27 @@ NtagTelEmulatorModel* ntag_tel_emulator_model_alloc(){
 
     instance->nfc_file_name = furi_string_alloc();
 
+    // NFC Device
     instance->nfc = nfc_device_alloc();
+
+    // NFC Data
+    // TODO: Move elsewhere
+    FURI_LOG_D("NtagEmulator", "Setting up NFC Device...");
+    NfcDeviceData* dev_data = &instance->nfc->dev_data;
+    FuriHalNfcDevData* nfc_data = &dev_data->nfc_data;
+    
+    nfc_data->uid_len = 7;
+    uint8_t new_uid[] = {0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+    memcpy(&nfc_data->uid, &new_uid, 7);
+
+    nfc_data->atqa[0] = 0x00;
+    nfc_data->atqa[1] = 0x44;
+
+    nfc_data->sak = 0x00;
+
+    dev_data->mf_ul_data.data_size = 4;
+    uint8_t new_data[] = {0xd3, 0x4d, 0xbe, 0xef};
+    memcpy(&dev_data->mf_ul_data.data, &new_data, 4);
 
     return instance;
 }
@@ -21,17 +41,22 @@ void ntag_tel_emulator_model_free(NtagTelEmulatorModel* instance){
 NtagTelEmulatorData* ntag_tel_emulator_data_alloc() {
     NtagTelEmulatorData* instance = malloc(sizeof(NtagTelEmulatorData));
     
+    // Scene Manager
     instance->scene_manager = scene_manager_alloc(&nte_scene_manager_handlers, instance);
     
+    // View Dispatcher
     instance->view_dispatcher = view_dispatcher_alloc();
     view_dispatcher_enable_queue(instance->view_dispatcher);
+
+    // Dialog
     instance->dialog_ex = dialog_ex_alloc();
     view_dispatcher_add_view(instance->view_dispatcher, 
             NtagTelEmulatorViewDialog, 
             dialog_ex_get_view(instance->dialog_ex));
 
-
+    //Model
     instance->model = ntag_tel_emulator_model_alloc();
+
     return instance;
 }
 
