6f530643a42018e83ac607eff8d4d69f550a7e18
Aaron <1330844+sesim-nov@users.noreply.github.com>
2023-07-17 12:55:06 -0400

    Adding scene for text input.
    Just a skeleton for now.

---
+++ scene_number_entry.c
@@ -0,0 +1,13 @@
+#include "ntag_tel_emulator.h"
+
+void scene_get_number_on_enter(void* ctx){
+    UNUSED(ctx);
+}
+bool scene_get_number_on_event(void* ctx, SceneManagerEvent event){
+    UNUSED(ctx);
+    UNUSED(event);
+    return false;
+}
+void scene_get_number_on_exit(void* ctx){
+    UNUSED(ctx);
+}
